f584f5ea2f34a6db8d7d3c2783f8637129a1b84b
Blagovest-Todorov <blagovest.stod@abv.bg>
2022-05-19 17:14:25 +0300

    Add project files.

---
+++ WebDriverTestsDemo/WikipediaTest.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+// Create new Chrome browser instance
+var driver = new ChromeDriver();
+
+// Navigate to wikipedia
+driver.Url = "https://wikipedia.org";
+
+Console.WriteLine("CURRENT TITLE: " + driver.Title);
+
+// locate search field by Id
+var searchField = driver.FindElement(By.Id("searchInput"));
+
+// click on element
+searchField.Click();
+
+//write QA and press enter btn
+searchField.SendKeys("Quality Assurance" + Keys.Enter);
+
+Console.WriteLine("Title after search: " + driver.Title);
+
+
+// close the browser
+//driver.Quit();
+
